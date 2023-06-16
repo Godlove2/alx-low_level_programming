b48fa618bc261e13768d0ad62284ba01383f6755
root <root@DESKTOP-LH7IV0E>
2023-06-16 09:09:24 -0700

    Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,24 +1,10 @@
-Here is a C program that prints exactly "with proper grammar, but the outcome is a piece of art," followed by a new line character using the `printf` function:
-
-```c
 #include <stdio.h>
-
+/**
+ * main - A program that prints a line using the printf function
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-	    printf("with proper grammar, but the outcome is a piece of art,\n");
-	        return (0);
-}
-```
-
-This program uses the `printf` function to print the string "with proper grammar, but the outcome is a piece of art," followed by a new line character. The `return (0)` statement at the end of the program ensures that it ends with the value 0. To compile this program without warnings when using the `-Wall` gcc option, you can include the following line at the beginning of your program:
-
-```c
-#include <stdio.h>
-```
-
-I hope this helps! Let me know if you have any other questions.
-
-Source: Conversation with Bing, 6/16/2023
-(1) mecomonteshbtn/C-low_level_programming - GitHub. https://github.com/mecomonteshbtn/C-low_level_programming.
-(2) holbertonschool-low_level_programming/0x00-hello_world/README ... - GitHub. https://github.com/bdbaraban/holbertonschool-low_level_programming/blob/master/0x00-hello_world/README.md.
-(3) c - program that prints itself, how does it work? - Stack Overflow. https://stackoverflow.com/questions/7690862/program-that-prints-itself-how-does-it-work.
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}	
