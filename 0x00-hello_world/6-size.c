f22a82dd42d51619bea400654c38594bfc2f06b3
Emmanuel Yankah <emmanuelyankah38@gmail.com>
2023-06-17 01:48:44 -0700

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Program that prints the size of the various types of computers
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	printf("Size of char: %lu byte(s)\n", sizeof(char)
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of long: %lu byte(s)\n", sizeof(long));
+	printf("Size of long long: %lu byte(s)\n", sizeof(long long));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
+
+	return (0);
+}
