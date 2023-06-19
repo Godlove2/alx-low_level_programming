27ed057f0b39f864e7a3dcb1b90207a0e79d5813
Emmanuel Yankah <emmanuelyankah38@gmail.com>
2023-06-19 03:56:32 -0700

    3-print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,7 +11,8 @@ int main(void)
 
 	for (low = 'a'; low <= 'z'; low++)
 	putchar(low);
-        for (low = 'A'; low <= 'Z'; low++)
+	
+	for (low = 'A'; low <= 'Z'; low++)
 	putchar('\n');
 	return (0);
 }
