8b290575e52b6dc6956554d78b0a85279852469f
Emmanuel Yankah <emmanuelyankah38@gmail.com>
2023-06-19 02:46:47 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - The c program that prints alphabetic
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
